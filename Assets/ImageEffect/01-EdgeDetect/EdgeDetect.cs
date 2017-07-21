ec7820a5d18a6ec7b4c2e02a7ad83db48602db29
范鑫磊 <wormlucky@gmail.com>
2017-07-21 19:22:27 +0800

    update

---
+++ Assets/ImageEffect/01-EdgeDetect/EdgeDetect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class EdgeDetect : MonoBehaviour {
+
+	public Material material;
+
+
+	public float EdgeOnly = 1;
+
+	public Color EdgeColor = Color.black;
+
+	public Color BackgroundColor = Color.white;
+
+	void OnStart(){
+		if (!SystemInfo.supportsImageEffects || null == material || 
+			null == material.shader || !material.shader.isSupported){
+			enabled = false;
+			return;
+		}	
+	}
+
+	void OnRenderImage(RenderTexture rt1,RenderTexture rt2){
+		material.SetFloat ("_EdgeOnly", EdgeOnly);
+		material.SetColor ("_EdgeColor", EdgeColor);
+		material.SetColor ("_BackgroundColor", BackgroundColor);
+		Graphics.Blit (rt1, rt2, material);
+	}
+}
