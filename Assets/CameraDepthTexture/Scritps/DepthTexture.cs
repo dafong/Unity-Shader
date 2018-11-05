7eb4bd6d80961452d09f51bb02b1b7c05b36ec7b
fxl <wormlucky@gmail.com>
2018-11-05 13:14:59 +0800

    Add CameraDepth Test

---
+++ Assets/CameraDepthTexture/Scritps/DepthTexture.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//[ExecuteInEditMode]
+public class DepthTexture : MonoBehaviour {
+
+    public Material material;
+
+    private void Awake() {
+        //Camera.main.depthTextureMode = DepthTextureMode.Depth;
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        Graphics.Blit(source, destination, material);
+    }
+
+}
